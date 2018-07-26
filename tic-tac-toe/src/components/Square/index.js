6833cd3a88dd2e47e2071dfa7997b80ccd67b83a
Priya Hiremath <priyahrish18@gmail.com>
2018-07-27 11:58:19 +1200

    Improve the board squares

--- tic-tac-toe/src/components/Square/index.js
+++ tic-tac-toe/src/components/Square/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledSquare = styled.div`
-  border-color: hsla(0, o%, 0%, 0.2);
+  border-color: hsla(0, o%, o%, 0.2);
   border-style: solid;
-  border-width: 2px;
-  color: gray;
+  border-width: 0 ${props => (props.index % 3 === 2 ? 0 : '2px')}
+    ${props => (props.index < 6 ? '2px' : 0)} 0;
+  color: ${props =>
+    props.player === 'x' ? 'hsl(46, 95%, 59%)' : 'hsl(0, 0%, 0%)'};
   font-size: 16vh;
   font-weight: bold;
   text-align: center;
