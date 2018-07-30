c165486e0ceb7b15fb12b0eccfdd89e6b0a408c3
Priya Hiremath <priyahrish18@gmail.com>
2018-07-30 12:24:37 +1200

    Add squareClicked action creator

---
+++ tic-tac-toe/src/state/actions/index.js
@@ -0,0 +1,11 @@
+import { SQUARE_CLICKED } from '..'
+
+function squareClicked (square) {
+  return {
+    type: SQUARE_CLICKED,
+    payload: {
+      square
+    }
+  }
+}
+export { squareClicked }
---
+++ tic-tac-toe/src/state/actions/index.spec.js
@@ -0,0 +1,18 @@
+// src/state/actions/index.spec.js
+import { squareClicked } from '.'
+import { SQUARE_CLICKED } from '..'
+
+describe('state: actions', () => {
+  describe('squareClicked', () => {
+    it('produces the correct action for clicking a square', () => {
+      const square = 4
+
+      expect(squareClicked(square)).toMatchObject({
+        type: SQUARE_CLICKED,
+        payload: {
+          square: 4
+        }
+      })
+    })
+  })
+})
